de3882eed3eeb0091e522330e300bd11b31a9c0a
Paraskevas Petsanas <42607603+ppel123@users.noreply.github.com>
2022-08-14 17:48:11 +0300

    open multiple webpages

---
+++ Actions/2. Open multiple websites/open_multiple_websites.ps1
@@ -0,0 +1,34 @@
+﻿# define websites txt file
+$links = Get-Content .\websites.txt
+$counter = 0
+
+foreach ($link in $links){
+
+    # Microsoft Edge
+    # Start-Process msedge.exe -ArgumentList "-url $link"
+
+    # Open in Private Window
+    # Start-Process msedge.exe -ArgumentList "-url $link -inprivate"
+    
+    # Mozilla Firefox -> firefox.exe parameters https://wiki.mozilla.org/Firefox/CommandLineOptions#-private
+    Start-Process 'C:\Program Files\Mozilla Firefox\firefox.exe' -ArgumentList "-url $link"
+
+    # Open in Private Window
+    # If ($counter -eq 0){
+    #     Start-Process 'C:\Program Files\Mozilla Firefox\firefox.exe' -ArgumentList "-private-window"
+    #     Start-Sleep -Seconds 1
+    #     Start-Process 'C:\Program Files\Mozilla Firefox\firefox.exe' -ArgumentList "-private-window $link"
+    # }
+    # Else{
+    #     Start-Process 'C:\Program Files\Mozilla Firefox\firefox.exe' -ArgumentList "-private-window $link"
+    # }
+    
+    # Google Chrome
+    # Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList '-url $link'
+
+    # Open in Private Window
+    # Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList '--incognito -url $link'
+    
+    Start-Sleep -Seconds 1
+    $counter = $counter + 1
+}
